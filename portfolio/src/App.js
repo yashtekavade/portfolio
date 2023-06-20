dff84e96e1a5a064f5ec82097d2d1c7d3e9e0bc5
bishh <yashtekavade@gmail.com>
2023-06-21 00:42:56 +0530

    home and about component created

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 const App = () => {
   return (
     <>
       <Header />
+
+      <main className="main">
+        <Home />
+        <About />
+      </main>
     </>
   );
 };
