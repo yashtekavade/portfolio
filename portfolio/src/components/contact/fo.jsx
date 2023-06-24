82d62c0598f011f7d855e2e249617cfeb87e531c
bishh <yashtekavade@gmail.com>
2023-06-24 13:14:57 +0530

    contact changes

--- portfolio/src/components/contact/fo.jsx
+++ portfolio/src/components/contact/fo.jsx
@@ -5,10 +5,12 @@ const fo = () => {
     <div className="contact__info grid">
       <div className="contact__box">
         <i class="uil uil-map-marker"></i>
+        <h4>Location</h4>
       </div>
       <h2 className="section__subtitle">Pune,India</h2>
       <div className="contact__box">
         <i class="uil uil-envelope-alt"></i>
+        <h4>Mail</h4>
       </div>
       <h2 className="section__subtitle">yashtekavade@gmail.com</h2>
     </div>
